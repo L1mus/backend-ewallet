d91b403dcb5f45006fd5326efe5fe3448bcffb19
L1mus <ali.firefox@yahoo.com>
2026-05-17 23:49:42 +0700

    feat: Route auth, create AuthRouter Fn

---
+++ internal/router/auth.route.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"github.com/L1mus/backend-ewallet/internal/controller"
+	"github.com/L1mus/backend-ewallet/internal/repository"
+	"github.com/L1mus/backend-ewallet/internal/service"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func AuthRouter(router *gin.Engine, db *pgxpool.Pool) {
+	authRouter := router.Group("/auth")
+
+	authRepository := repository.NewAuthRepository(db)
+	authService := service.NewAuthService(authRepository)
+	authController := controller.NewAuthController(authService)
+	authRouter.POST("/register", authController.Register)
+}
