7df877b92727a7cead98ac9279991bbe7013056c
L1mus <ali.firefox@yahoo.com>
2026-05-17 23:46:18 +0700

    feat: main router, first Endpoint is Auth router. add middleware CORS and fallback Route "No route"

---
+++ internal/router/main.router.go
@@ -0,0 +1,25 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/L1mus/backend-ewallet/internal/dto"
+	"github.com/L1mus/backend-ewallet/internal/middleware"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func InitRouter(router *gin.Engine, db *pgxpool.Pool) {
+	// middleware global
+	router.Use(middleware.CORSMiddleware())
+	// router.METHOD(endpoint, callback)
+	AuthRouter(router, db)
+
+	router.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(http.StatusNotFound, dto.ResponseError{
+			Status:  "Error",
+			Message: "Invalid Route",
+			Error:   "route not found",
+		})
+	})
+}
