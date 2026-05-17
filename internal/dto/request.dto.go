ddf67a7aef601850128d114667fce2906f8d8cfc
L1mus <ali.firefox@yahoo.com>
2026-05-17 23:26:45 +0700

    feat: create Schema DTO request, first create RegisterRequest DTO

---
+++ internal/dto/request.dto.go
@@ -0,0 +1,8 @@
+package dto
+
+type RegisterRequest struct {
+	FullName        string `json:"full_name" binding:"required,min=3"`
+	Email           string `json:"email" binding:"required,email"`
+	Password        string `json:"password" binding:"required,min=8"`
+	ConfirmPassword string `json:"confirm_password" binding:"required,eqfield=Password"`
+}
