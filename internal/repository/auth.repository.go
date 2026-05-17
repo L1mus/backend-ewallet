e582b996c6f42d7217d9311880029740aa69bc30
L1mus <ali.firefox@yahoo.com>
2026-05-18 00:43:31 +0700

    refactor: forgot to insert wallet data, and create Rollback Fn from context for handle Inconsistent data

--- internal/repository/auth.repository.go
+++ internal/repository/auth.repository.go
@@ -2,11 +2,11 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"log"
-	"strings"
 
-	"github.com/L1mus/backend-ewallet/internal/appError"
 	"github.com/L1mus/backend-ewallet/internal/model"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -20,24 +20,46 @@ func NewAuthRepository(db *pgxpool.Pool) *AuthRepository {
 	}
 }
 
-func (r *AuthRepository) Register(ctx context.Context, full_name, email, hash_password string) (model.User, error) {
-	sql := `INSERT INTO users (full_name,email,hash_password) VALUES($1,$2,$3) RETURNING id, full_name, email, created_at`
-	args := []any{full_name, email, hash_password}
-	var user model.User
-	if err := r.db.QueryRow(ctx, sql, args...).Scan(&user.Id, &user.FullName, &user.Email, &user.CreatedAt); err != nil {
+func (r *AuthRepository) Register(ctx context.Context, fullName, email, hashPassword string) (model.User, error) {
+	tx, err := r.db.Begin(ctx)
+	if err != nil {
 		return model.User{}, err
 	}
+	defer func(tx pgx.Tx, ctx context.Context) {
+		err := tx.Rollback(ctx)
+		if err != nil {
+			log.Println("error: ", err)
+		}
+	}(tx, ctx)
+
+	sqlUser := `INSERT INTO users (full_name,email,hash_password) VALUES($1,$2,$3) RETURNING id, full_name, email, created_at`
+	args := []any{fullName, email, hashPassword}
+	var user model.User
+	if err := tx.QueryRow(ctx, sqlUser, args...).Scan(&user.Id, &user.FullName, &user.Email, &user.CreatedAt); err != nil {
+		return model.User{}, err
+	}
+
+	fmt.Println(user.Id)
+
+	sqlWallet := `INSERT INTO wallet (user_id, balance, updated_at) VALUES($1, 0.00, NOW())`
+	if _, err := tx.Exec(ctx, sqlWallet, user.Id); err != nil {
+		return model.User{}, err
+	}
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		return model.User{}, err
+	}
+
 	return user, nil
 }
 
-func (r *AuthRepository) CheckEmailExist(ctx context.Context, email string) error {
-	sql := `SELECT email FROM users WHERE email=$1 AND deleted_at = NULL`
+func (r *AuthRepository) CheckEmailExist(ctx context.Context, email string) (bool, error) {
+	var count int
+	sql := `SELECT COUNT(1) FROM users WHERE email=$1 AND deleted_at IS NULL`
 	args := []any{email}
-	if _, err := r.db.Exec(ctx, sql, args...); err != nil {
-		log.Println("Error :", err.Error())
-		if strings.Contains(err.Error(), "duplicate") {
-			return appError.EmailAlreadyExists
-		}
+	if err := r.db.QueryRow(ctx, sql, args...).Scan(&count); err != nil {
+		return false, err
 	}
-	return nil
+	return count > 0, nil
 }
