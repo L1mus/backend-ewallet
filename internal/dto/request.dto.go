3dec12935bc519aaaab5171a125043447ad59989
l1mus <ali.firefox@yahoo.co.id>
2026-05-19 21:39:31 +0700

    feat: add tag example

--- internal/dto/request.dto.go
+++ internal/dto/request.dto.go
@@ -1,13 +1,13 @@
 package dto
 
 type RegisterRequest struct {
-	FullName        string `json:"full_name" binding:"required,min=3"`
-	Email           string `json:"email" binding:"required,email"`
-	Password        string `json:"password" binding:"required,min=8"`
-	ConfirmPassword string `json:"confirm_password" binding:"required,eqfield=Password"`
+	FullName        string `json:"full_name" binding:"required,min=3" example:"John Doe"`
+	Email           string `json:"email" binding:"required,email" example:"example@mail.com"`
+	Password        string `json:"password" binding:"required,min=8" example:"example123"`
+	ConfirmPassword string `json:"confirm_password" binding:"required,eqfield=Password" example:"example123"`
 }
 
 type LoginRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password" binding:"required,min=8"`
+	Email    string `json:"email" example:"example@mail.com"`
+	Password string `json:"password" example:"example123"`
 }
