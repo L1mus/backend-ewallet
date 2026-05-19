a18f3539462446dc9d5a29bf1b3f4e0f1434ac10
l1mus <ali.firefox@yahoo.co.id>
2026-05-19 13:06:37 +0700

    refactor : unnecessary code return ginFunction

--- internal/middleware/cors.middleware.go
+++ internal/middleware/cors.middleware.go
@@ -8,26 +8,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func CORSMiddleware() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		origin := ctx.GetHeader("Origin")
+func CORSMiddleware(ctx *gin.Context) {
+	origin := ctx.GetHeader("Origin")
 
-		AllowOrigins := []string{"http://localhost:5173"}
-		AllowHeaders := []string{"Origin", "Content-Type", "Authorization"}
-		AllowMethods := []string{http.MethodGet, http.MethodPost, http.MethodPatch, http.MethodDelete, http.MethodOptions}
+	AllowOrigins := []string{"http://localhost:5173"}
+	AllowHeaders := []string{"Origin", "Content-Type", "Authorization"}
+	AllowMethods := []string{http.MethodGet, http.MethodPost, http.MethodPatch, http.MethodDelete, http.MethodOptions}
 
-		if slices.Contains(AllowOrigins, origin) {
-			ctx.Header("Access-Control-Allow-Origin", origin)
-		}
-
-		ctx.Header("Access-Control-Allow-Headers", strings.Join(AllowHeaders, ", "))
-		ctx.Header("Access-Control-Allow-Methods", strings.Join(AllowMethods, ", "))
-
-		if ctx.Request.Method == http.MethodOptions {
-			ctx.AbortWithStatus(http.StatusNoContent)
-			return
-		}
-
-		ctx.Next()
+	if slices.Contains(AllowOrigins, origin) {
+		ctx.Header("Access-Control-Allow-Origin", origin)
 	}
+
+	ctx.Header("Access-Control-Allow-Headers", strings.Join(AllowHeaders, ", "))
+	ctx.Header("Access-Control-Allow-Methods", strings.Join(AllowMethods, ", "))
+
+	if ctx.Request.Method == http.MethodOptions {
+		ctx.AbortWithStatus(http.StatusNoContent)
+		return
+	}
+
+	ctx.Next()
 }
