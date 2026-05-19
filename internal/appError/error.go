d5dbcbeffcc30d7583256e3163960a2f33899a88
l1mus <ali.firefox@yahoo.co.id>
2026-05-19 12:57:51 +0700

    feat: add new error message EmailOrPassWrong and UserNotFound

--- internal/appError/error.go
+++ internal/appError/error.go
@@ -5,4 +5,6 @@ import "errors"
 var (
 	EmailAlreadyExists = errors.New("email already exists")
 	InvalidEmailFormat = errors.New("invalid email format")
+	EmailOrPassWrong   = errors.New("wrong email or password")
+	UserNotFound       = errors.New("profile not found")
 )
