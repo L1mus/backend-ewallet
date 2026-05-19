5887f37f6898847ed5ed34dedd292aff766d7bf1
l1mus <ali.firefox@yahoo.co.id>
2026-05-19 13:05:23 +0700

    feat : add repository user

---
+++ internal/repository/user.repository.go
@@ -0,0 +1,29 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/L1mus/backend-ewallet/internal/model"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type UserRepository struct {
+	db *pgxpool.Pool
+}
+
+func NewUserRepository(db *pgxpool.Pool) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
+
+func (r *UserRepository) GetUserProfile(ctx context.Context, id int) (model.UserProfile, error) {
+	sql := `SELECT id,full_name,email,phone,profile_picture_url FROM users WHERE id = $1`
+
+	args := []any{id}
+	var user model.UserProfile
+	if err := r.db.QueryRow(ctx, sql, args...).Scan(&user.Id, &user.FullName, &user.Email, &user.Phone, &user.ProfilePictureURL); err != nil {
+		return model.UserProfile{}, err
+	}
+	return user, nil
+}
