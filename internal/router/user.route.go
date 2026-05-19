b3fa3fa16865a77a56f9a58d189ade6ceba64995
l1mus <ali.firefox@yahoo.co.id>
2026-05-19 13:05:44 +0700

    feat : add route user

---
+++ internal/router/user.route.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"github.com/L1mus/backend-ewallet/internal/controller"
+	"github.com/L1mus/backend-ewallet/internal/middleware"
+	"github.com/L1mus/backend-ewallet/internal/repository"
+	"github.com/L1mus/backend-ewallet/internal/service"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func UserRouter(router *gin.Engine, db *pgxpool.Pool) {
+	userRouter := router.Group("/users")
+
+	userRepository := repository.NewUserRepository(db)
+	userService := service.NewUserService(userRepository)
+	userController := controller.NewUserController(userService)
+
+	userRouter.GET("/profile", middleware.VerifyToken, userController.GetUserProfile)
+}
