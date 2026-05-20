5e943bcb1d3be6fe5f61cd1c16097e8633653c88
l1mus <ali.firefox@yahoo.co.id>
2026-05-21 00:02:18 +0700

    feat: add Get user dashboard repository

--- internal/repository/user.repository.go
+++ internal/repository/user.repository.go
@@ -27,3 +27,18 @@ func (r *UserRepository) GetUserProfile(ctx context.Context, id int) (model.User
 	}
 	return data, nil
 }
+
+func (r *UserRepository) GetUserDashboard(ctx context.Context, id int) (model.UserDashboard, error) {
+	sql := `
+			SELECT  w.balance, SUM(CASE WHEN t.type = 'income' AND t.status = 'success' THEN t.amount ELSE 0 END) AS total_income, SUM(CASE WHEN t.type = 'expense' AND t.status = 'success' THEN t.amount ELSE 0 END) AS total_expense
+			FROM wallet w
+			LEFT JOIN transactions t ON w.user_id = t.user_id
+			WHERE w.user_id = $1
+			GROUP BY w.balance`
+	args := []any{id}
+	var data model.UserDashboard
+	if err := r.db.QueryRow(ctx, sql, args...).Scan(&data.Balance, &data.TotalIncome, &data.TotalExpenses); err != nil {
+		return model.UserDashboard{}, err
+	}
+	return data, nil
+}
