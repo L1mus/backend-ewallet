d148cb7a2de1d297c140953fd391c7d573d83f80
L1mus <ali.firefox@yahoo.com>
2026-05-23 18:39:06 +0700

    feat : add Update Profile to User router

--- internal/router/user.route.go
+++ internal/router/user.route.go
@@ -21,6 +21,7 @@ func UserRouter(router *gin.Engine, db *pgxpool.Pool) {
 	userRouter.GET("/report", middleware.VerifyToken, userController.GetTransactionReport)
 	userRouter.GET("/transfer", middleware.VerifyToken, userController.FindReceiver)
 	userRouter.GET("/transactions", middleware.VerifyToken, userController.GetTransactionHistory)
+	userRouter.PATCH("/profile", middleware.VerifyToken, userController.EditProfile)
 	userRouter.PATCH("/pin", middleware.VerifyToken, userController.EditPin)
 	userRouter.PATCH("/password", middleware.VerifyToken, userController.EditPassword)
 }
