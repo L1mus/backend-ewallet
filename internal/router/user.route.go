51bd44c23bbe9f522bc34ba4d67c88f93179dd63
L1mus <ali.firefox@yahoo.com>
2026-05-23 10:17:26 +0700

    feat add route for GetTransactionHistory

--- internal/router/user.route.go
+++ internal/router/user.route.go
@@ -20,4 +20,5 @@ func UserRouter(router *gin.Engine, db *pgxpool.Pool) {
 	userRouter.GET("/dashboard", middleware.VerifyToken, userController.GetUserDashboard)
 	userRouter.GET("/report", middleware.VerifyToken, userController.GetTransactionReport)
 	userRouter.GET("/transfer", middleware.VerifyToken, userController.FindReceiver)
+	userRouter.GET("/transactions", middleware.VerifyToken, userController.GetTransactionHistory)
 }
