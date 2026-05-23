cbd1df1462cf7d11985c9866de1ef10a049a17c3
L1mus <ali.firefox@yahoo.com>
2026-05-23 18:36:56 +0700

    feat : add Edit Password to User router

--- internal/router/user.route.go
+++ internal/router/user.route.go
@@ -22,4 +22,5 @@ func UserRouter(router *gin.Engine, db *pgxpool.Pool) {
 	userRouter.GET("/transfer", middleware.VerifyToken, userController.FindReceiver)
 	userRouter.GET("/transactions", middleware.VerifyToken, userController.GetTransactionHistory)
 	userRouter.PATCH("/pin", middleware.VerifyToken, userController.EditPin)
+	userRouter.PATCH("/password", middleware.VerifyToken, userController.EditPassword)
 }
