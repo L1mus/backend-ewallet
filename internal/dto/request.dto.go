f3139f1792f070f6071c059d4c91e545cdc15911
L1mus <ali.firefox@yahoo.com>
2026-05-23 18:29:35 +0700

    feat : add DTO for request Edit PIN,password,and profile

--- internal/dto/request.dto.go
+++ internal/dto/request.dto.go
@@ -27,4 +27,21 @@ type ReceiverQuery struct {
 	Page   string `form:"page" default:"1" example:"1"`
 	Search string `form:"search" example:"John Doe"`
 }
+
+type EditPinRequest struct {
+	CurrentPin    string `json:"current_pin"    binding:"required,len=6"`
+	NewPin        string `json:"new_pin"        binding:"required,len=6"`
+	ConfirmNewPin string `json:"confirm_new_pin" binding:"required,eqfield=NewPin"`
+}
+
+type EditPasswordRequest struct {
+	CurrentPassword string `json:"current_password" binding:"required"`
+	NewPassword     string `json:"new_password"     binding:"required,min=8"`
+	ConfirmPassword string `json:"confirm_password" binding:"required,eqfield=NewPassword"`
+}
+
+type EditProfileRequest struct {
+	FullName          string  `json:"full_name"           binding:"required,min=3"`
+	Phone             *string `json:"phone"`
+	ProfilePictureURL *string `json:"profile_picture_url"`
 }
