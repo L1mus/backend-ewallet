6196e961ed1f14abff33b36f02134e09243fcd11
L1mus <ali.firefox@yahoo.com>
2026-05-27 19:03:08 +0700

    refactor : static directory path

--- internal/router/main.router.go
+++ internal/router/main.router.go
@@ -18,8 +18,8 @@ func InitRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
 	//swagger docs
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	// router.METHOD(endpoint, callback)
-	router.Static("/img", "/public/img")
-	AuthRouter(router, db)
+	router.Static("/img", "public/img")
+	AuthRouter(router, db, rdb)
 	UserRouter(router, db, rdb)
 	TransactionRouter(router, db)
 
