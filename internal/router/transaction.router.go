34ac5047ccb643e7fd1a9a16e65d91a3e32315b0
L1mus <ali.firefox@yahoo.com>
2026-05-27 22:30:46 +0700

    feat: redis implementation

--- internal/router/transaction.router.go
+++ internal/router/transaction.router.go
@@ -7,14 +7,16 @@ import (
 	"github.com/L1mus/backend-ewallet/internal/service"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 )
 
-func TransactionRouter(router *gin.Engine, db *pgxpool.Pool) {
+func TransactionRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
 	transactionRouter := router.Group("/transactions")
+	transactionRouter.Use(middleware.VerifyToken, middleware.CheckBlacklist(rdb))
 
 	transactionRepo := repository.NewTransactionRepository()
 	transactionService := service.NewTransactionService(transactionRepo, db)
 	transactionController := controller.NewTransactionController(transactionService)
 
-	transactionRouter.POST("/transfer", middleware.VerifyToken, transactionController.CreateTransfer)
+	transactionRouter.POST("/transfer", transactionController.CreateTransfer)
 }
