637ec79192a586ba1504e4cf291a2b6e5103bdcc
L1mus <ali.firefox@yahoo.com>
2026-05-27 19:07:25 +0700

    refactor : change fn controller route, and delete route users/avatar

--- internal/router/user.router.go
+++ internal/router/user.router.go
@@ -22,8 +22,7 @@ func UserRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
 	userRouter.GET("/report", middleware.VerifyToken, userController.GetTransactionReport)
 	userRouter.GET("/transfer", middleware.VerifyToken, userController.FindReceiver)
 	userRouter.GET("/transactions", middleware.VerifyToken, userController.GetTransactionHistory)
-	userRouter.PATCH("/profile", middleware.VerifyToken, userController.EditProfile)
-	userRouter.PATCH("/avatar", middleware.VerifyToken, userController.UploadProfilePicture)
+	userRouter.PUT("/profile", middleware.VerifyToken, userController.UpdateProfile)
 	userRouter.PATCH("/pin", middleware.VerifyToken, userController.EditPin)
 	userRouter.PATCH("/password", middleware.VerifyToken, userController.EditPassword)
 }
