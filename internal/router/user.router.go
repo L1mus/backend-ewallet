f14e551ba82c8f6715e07ec79241a3c7dc62f39f
L1mus <ali.firefox@yahoo.com>
2026-05-27 22:32:52 +0700

    refactor : add middleware CheckBlacklist for authentication

--- internal/router/user.router.go
+++ internal/router/user.router.go
@@ -12,17 +12,17 @@ import (
 
 func UserRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
 	userRouter := router.Group("/users")
-
+	userRouter.Use(middleware.VerifyToken, middleware.CheckBlacklist(rdb))
 	userRepository := repository.NewUserRepository(db)
 	userService := service.NewUserService(userRepository, rdb)
 	userController := controller.NewUserController(userService)
 
-	userRouter.GET("/profile", middleware.VerifyToken, userController.GetUserProfile)
-	userRouter.GET("/dashboard", middleware.VerifyToken, userController.GetUserDashboard)
-	userRouter.GET("/report", middleware.VerifyToken, userController.GetTransactionReport)
-	userRouter.GET("/transfer", middleware.VerifyToken, userController.FindReceiver)
-	userRouter.GET("/transactions", middleware.VerifyToken, userController.GetTransactionHistory)
-	userRouter.PUT("/profile", middleware.VerifyToken, userController.UpdateProfile)
-	userRouter.PATCH("/pin", middleware.VerifyToken, userController.EditPin)
-	userRouter.PATCH("/password", middleware.VerifyToken, userController.EditPassword)
+	userRouter.GET("/profile", userController.GetUserProfile)
+	userRouter.GET("/dashboard", userController.GetUserDashboard)
+	userRouter.GET("/report", userController.GetTransactionReport)
+	userRouter.GET("/transfer", userController.FindReceiver)
+	userRouter.GET("/transactions", userController.GetTransactionHistory)
+	userRouter.PUT("/profile", userController.UpdateProfile)
+	userRouter.PATCH("/pin", userController.EditPin)
+	userRouter.PATCH("/password", userController.EditPassword)
 }
