7d31a5801bfbe526920c6c507e98196d846cc8d6
L1mus <ali.firefox@yahoo.com>
2026-06-01 19:04:36 +0700

    fix: bug route login

--- internal/router/auth.router.go
+++ internal/router/auth.router.go
@@ -17,7 +17,7 @@ func AuthRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
 	authService := service.NewAuthService(authRepository, rdb)
 	authController := controller.NewAuthController(authService)
 
-	authRouter.POST("/", authController.Login)
+	authRouter.POST("", authController.Login)
 	authRouter.POST("/register", authController.Register)
 	authRouter.POST("/logout", middleware.VerifyToken, middleware.CheckBlacklist(rdb), authController.Logout)
 	authRouter.POST("/forgot-password", authController.ForgotPassword)
