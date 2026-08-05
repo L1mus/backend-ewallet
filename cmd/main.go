604da62161369ec1b4e8e9ebdf6ee52e3368ec8d
L1mus <ali.firefox@yahoo.com>
2026-08-05 14:51:51 +0700

    feat: add fn load configuration and fn Database connection to main

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/L1mus/backend-ewallet/internal/infrastructure/config"
+	"github.com/L1mus/backend-ewallet/internal/infrastructure/database/postgres"
 	"github.com/joho/godotenv"
 )
 
@@ -14,6 +17,26 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Printf("Failed to load Environment Variable")
 	}
+
+	// DB configuration
+	configuration, err := config.LoadConfiguration()
+	if err != nil {
+		log.Printf("Failed to load configuration database")
+	}
+
+	// DB connection
+	db, err := postgres.DBConnection(configuration)
+	if err != nil {
+		log.Fatalf("Connection error : %s", err)
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Fatal("Failed to close database")
+		}
+	}(db)
+	log.Println("Database Connected")
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("POST /hello", func(w http.ResponseWriter, r *http.Request) {
