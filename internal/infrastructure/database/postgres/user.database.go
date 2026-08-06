4ec8fbef19787e12b04159102b8c15173081374c
L1mus <ali.firefox@yahoo.co.id>
2026-08-06 09:30:12 +0700

    refactor: fn findbyemail return all information about user for reusable methode

--- internal/infrastructure/database/postgres/user.database.go
+++ internal/infrastructure/database/postgres/user.database.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
+	"log"
 
 	domainUser "github.com/L1mus/backend-ewallet/internal/domain/user"
 )
@@ -18,20 +20,34 @@ func NewUserRepository(db *sql.DB) domainUser.UserRepository {
 
 func (r *userRepository) FindByEmail(ctx context.Context, email string) (*domainUser.User, error) {
 	q := `
-	SELECT full_name,email,phone,profile_picture_url,created_at 
+	SELECT id, full_name, email, hash_password, hash_pin, phone, profile_picture_url, created_at
 	FROM users 
-	WHERE email = $1`
+	WHERE email = $1 AND deleted_at IS NULL`
 
 	var u domainUser.User
+	var hashPin, phone, profilePictureURL sql.NullString
 	row := r.db.QueryRowContext(ctx, q, email)
 
-	err := row.Scan(&u.FullName, &u.Email, u.Phone, u.ProfilePictureURL, u.CreatedAt)
+	err := row.Scan(
+		&u.ID,
+		&u.FullName,
+		&u.Email,
+		&u.HashPassword,
+		&hashPin,
+		&phone,
+		&profilePictureURL,
+		&u.CreatedAt,
+	)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, nil
 		}
 		return nil, err
 	}
+
+	u.HashPin = hashPin.String
+	u.Phone = phone.String
+	u.ProfilePictureURL = profilePictureURL.String
 
 	return &u, nil
 }
