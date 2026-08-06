287e667408ffb067b7d7b4b6ccdf2c49c96c9a97
L1mus <ali.firefox@yahoo.co.id>
2026-08-06 08:07:06 +0700

    Revert "feat: field atribut phone and ProfilePictureURL"
    This reverts commit f712de4fd78c7ec7390ff392de03ff5d49c248ab.

--- internal/domain/user/repository.go
+++ internal/domain/user/repository.go
@@ -3,6 +3,6 @@ package user
 import "context"
 
 type UserRepository interface {
-	FindByEmail(ctx context.Context, email string) (*User, error)
+	FindEmail(ctx context.Context, email string) (*User, error)
 	Save(ctx context.Context, u *User) error
 }
