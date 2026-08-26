905857a1006c2c9566e15405dddb60b63532af98
L1mus <ali.firefox@yahoo.co.id>
2026-08-26 10:32:28 +0700

    feat: create method register , to usecase for handle logic register account

--- internal/application/usecase/auth.usecase.go
+++ internal/application/usecase/auth.usecase.go
@@ -1,19 +1,44 @@
 package usecase
 
 import (
+	"context"
+
+	"github.com/L1mus/backend-ewallet/internal/application/dto"
 	domainUser "github.com/L1mus/backend-ewallet/internal/domain/user"
+	"github.com/L1mus/backend-ewallet/internal/infrastructure/crypto"
 )
 
 type AuthUseCase struct {
-	userRepo domainUser.UserRepository
+	userRepo       domainUser.UserRepository
+	PasswordHasher *crypto.Argon2idHasher
 }
 
-func NewAuthUseCase(repo domainUser.UserRepository) *AuthUseCase {
+func NewAuthUseCase(repo domainUser.UserRepository, hasher *crypto.Argon2idHasher) *AuthUseCase {
 	return &AuthUseCase{
-		userRepo: repo,
+		userRepo:       repo,
+		PasswordHasher: hasher,
 	}
 }
 
-func (c *AuthUseCase) Register() {
+func (c *AuthUseCase) Register(ctx context.Context, payload dto.RegisterRequest) error {
+	isEmailExist, err := c.userRepo.FindByEmail(ctx, payload.Email)
+	if err != nil {
+		return err
+	}
+	if isEmailExist != nil {
+		return domainUser.ErrEmailAlreadyExist
+	}
+	hashPassword := c.PasswordHasher.Generate(payload.Password)
 
+	newUser := &domainUser.User{
+		FullName:     payload.FullName,
+		Email:        payload.Email,
+		HashPassword: hashPassword,
+	}
+
+	if err := c.userRepo.Save(ctx, newUser); err != nil {
+		return err
+	}
+
+	return nil
 }
