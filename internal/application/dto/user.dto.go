6bc150c31f9d80c51874d632ad5bbe8db78c6482
L1mus <ali.firefox@yahoo.co.id>
2026-08-26 10:45:01 +0700

    refactor: implement go validator to struct tag for handle input validation request

--- internal/application/dto/user.dto.go
+++ internal/application/dto/user.dto.go
@@ -1,7 +1,8 @@
 package dto
 
 type RegisterRequest struct {
-	FullName string `json:"full_name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	FullName        string `json:"full_name" validate:"required,min=3,max=50"`
+	Email           string `json:"email" validate:"required,email"`
+	Password        string `json:"password" validate:"required,min=8"`
+	ConfirmPassword string `json:"confirmPassword" validate:"required,eqfield=Password"`
 }
