180558d3b5ab80b6da5a8b6fd5e9f2fe7db8bf1e
L1mus <ali.firefox@yahoo.co.id>
2026-08-26 10:30:57 +0700

    feat: add hasher instaces to auth service

--- internal/interfaces/http/router/auth.route.go
+++ internal/interfaces/http/router/auth.route.go
@@ -5,14 +5,16 @@ import (
 	"net/http"
 
 	"github.com/L1mus/backend-ewallet/internal/application/usecase"
+	"github.com/L1mus/backend-ewallet/internal/infrastructure/crypto"
 	"github.com/L1mus/backend-ewallet/internal/infrastructure/database/postgres"
 	"github.com/L1mus/backend-ewallet/internal/interfaces/http/handler"
 )
 
 func AuthRoute(mux *http.ServeMux, db *sql.DB) {
 
+	hasher := crypto.NewArgon2idHasher(64*1024, 1, 32, 16, 1)
 	userRepo := postgres.NewUserRepository(db)
-	authService := usecase.NewAuthUseCase(userRepo)
+	authService := usecase.NewAuthUseCase(userRepo, hasher)
 	authHandler := handler.NewAuthHandler(authService)
 
 	mux.Handle("POST /auth/register", handler.Wrap(authHandler.Register))
