5834561f06a36d908d3ea6b05c3815fa8355ce02
L1mus <ali.firefox@yahoo.co.id>
2026-08-26 10:29:41 +0700

    feat: create handler for register

--- internal/interfaces/http/handler/auth.handler.go
+++ internal/interfaces/http/handler/auth.handler.go
@@ -17,5 +17,15 @@ func NewAuthHandler(uc *usecase.AuthUseCase) *AuthHandler {
 }
 
 func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) error {
+	var payload dto.RegisterRequest
+	if err := request.DecodeAndValidate(r, &payload); err != nil {
+		return err
+	}
+
+	if err := h.authUseCase.Register(r.Context(), payload); err != nil {
+		return err
+	}
+
+	response.WriteSuccess(w, http.StatusCreated, nil)
 	return nil
 }
